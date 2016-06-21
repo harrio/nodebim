2729cef2b61b9588ad091445f0ce5872ad243a61
Harri Ohra-aho <harri.ohra-aho@solita.fi>
2016-06-21 17:43:30 +0300

    Teleporttilieriö

--- src/Teleporter.js
+++ src/Teleporter.js
@@ -1,22 +1,21 @@
 /* global THREE */
 
-const _createSphere = (x,y,z) => {
-    const geometry = new THREE.SphereGeometry( 0.5, 8, 8);
+const _createCone = (x,y,z) => {
+    const geometry = new THREE.CylinderGeometry(0.5, 0.5, 4, 8, 8);
     const material = new THREE.MeshBasicMaterial( {
-            color: 0xff00cc,
-            opacity: 0.1,
+            color: 0xffff00,
+            opacity: 0.4,
             transparent: true
           } );
-    const sphere = new THREE.Mesh( geometry, material );
-    sphere.position.x = x;
-    sphere.position.z = z;
-    sphere.position.y = y;
-    material.depthTest = false;
-    return sphere;
+    const cone = new THREE.Mesh(geometry, material);
+    cone.position.x = x;
+    cone.position.z = z;
+    cone.position.y = y;
+    return cone;
 };
 
 const createTeleporter = () => {
-  return _createSphere(0, 0, 0);
+  return _createCone(0, 0, 0);
 }
 
 export {
