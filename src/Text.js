6ea73515c520aa8eb30c8870ab2e8297b46138c7
Harri Ohra-aho <harri.ohra-aho@solita.fi>
2016-06-22 18:25:52 +0300

    Tekstikokeilu

--- src/Text.js
+++ src/Text.js
@@ -4,14 +4,14 @@ const makeTextSprite = (message, fontsize) => {
   let ctx, texture, sprite, spriteMaterial,
   canvas = document.createElement('canvas');
   ctx = canvas.getContext('2d');
-  ctx.font = fontsize + 'px Arial';
+  ctx.font = fontsize + 'px Helvetica';
 
   // setting canvas width/height before ctx draw, else canvas is empty
-  canvas.width = ctx.measureText(message).width;
+  //canvas.width = ctx.measureText(message).width;
   //canvas.height = fontsize * 2; // fontsize * 1.5
 
   // after setting the canvas width/height we have to re-set font to apply!?! looks like ctx reset
-  ctx.font = fontsize + 'px Arial';
+  ctx.font = fontsize + 'px Helvetica';
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
   ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
   ctx.fillText(message, 0, fontsize);
